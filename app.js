3d6525f7bd2b586e5a422c2f56146760a3e734fa
SamLangTen <samlangten@outlook.com>
2016-07-26 01:25:55 +0800

    Add: init project in vsc

---
+++ app.js
@@ -0,0 +1,8 @@
+/// <reference path="./typings/node.d.ts" />
+/// <reference path="./typings/express.d.ts" />
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
